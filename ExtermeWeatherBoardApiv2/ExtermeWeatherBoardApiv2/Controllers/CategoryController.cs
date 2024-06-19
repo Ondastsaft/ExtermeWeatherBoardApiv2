b5208838f9edd2b429454a3a42ce68630c8c93f9
Ondastsaft <kenneth.jansson@gmail.com>
2024-06-19 19:05:15 +0200

    Changed to Async and added AddCategory

--- ExtermeWeatherBoardApiv2/ExtermeWeatherBoardApiv2/Controllers/CategoryController.cs
+++ ExtermeWeatherBoardApiv2/ExtermeWeatherBoardApiv2/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using ExtermeWeatherBoardApiv2.Data;
 using ExtremeWeatherBoardApiv2.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,14 +18,21 @@ namespace ExtermeWeatherBoardApiv2.Controllers
         }
         // GET: api/<CategoryController>
         [HttpGet("GetAll")]
-        public List<Category?>? GetAll()
+        public async Task<List<Category>?> GetAllAsync()
         {
-            var categories = _context.Categories.ToList();
-            if (categories == null)
-            {
-                return null;
-            }
+            var categories = await _context.Categories.ToListAsync();
             return categories;
         }
+        [HttpPost("AddCategory")]
+        public async Task<IActionResult> AddCategory(Category category)
+        {
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }        
     }
 }
